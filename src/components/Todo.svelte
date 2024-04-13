d1e7cdf85e748094da3c2ba8a050433ba74dfe21
Arlei F. Farnetani Junior <farnetani@gmail.com>
2024-04-12 23:03:43 -0300

    Feat: added delete in item

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -5,6 +5,7 @@
 
     let task: string = ''
     // let tasks: string[] = []
+    let searchTerm: string = ''; // Termo de busca
 
     const addTask = () => {
         if (task) {
@@ -14,14 +15,21 @@
             task = ''
         }
     }
+
+    // Função para filtrar tarefas com base no termo de busca
+    $: filteredTasks = $tasks.filter(t => t.toLowerCase().includes(searchTerm.toLowerCase()));
+
 </script>
 
 <h1>Todo</h1>
 
+<input class="search-bar" type="text" bind:value={searchTerm} placeholder="Pesquisar..." />
+
 <form class="form" on:submit|preventDefault={addTask}>
-    <input bind:value={task} class="form__field" type="text" placeholder="Add a new todo" />
+    <input bind:value={task} class="form__field" type="text" placeholder="Adicione uma nova taefa" />
     <button class="form__button" type="submit">Add</button>
 </form>
 
 <!-- Podemos usar o $ para falar para a aplicação fazer um subscription, o $ já faz isso implicitamente, então não precisamos fazer isso manualmente. -->
-<TodoItem list={$tasks} onDelete={deleteTask} />
+<!-- <TodoItem list={$tasks} onDelete={deleteTask} /> -->
+<TodoItem list={filteredTasks} onDelete={deleteTask} />
